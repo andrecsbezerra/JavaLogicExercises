eb6db204d24674130aa03eba9288be29c3ea3ce7
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-09 19:29:13 -0300

    feat: add try catch

--- src/Exercise08.java
+++ src/Exercise08.java
@@ -1,23 +1,33 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Exercise08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean validInput = false;
         List<Integer> numbers = new ArrayList<>();
-        System.out.println("Type the first number");
-        int a = sc.nextInt();
-        System.out.println("Type the second number");
-        int b = sc.nextInt();
-        System.out.println("Type the third number");
-        int c = sc.nextInt();
+        while (!validInput) {
+            try {
+                System.out.println("Type the first number: ");
+                int a = sc.nextInt();
+                System.out.println("Type the second number: ");
+                int b = sc.nextInt();
+                System.out.println("Type the third number: ");
+                int c = sc.nextInt();
 
-        numbers.add(a);
-        numbers.add(b);
-        numbers.add(c);
+                numbers.add(a);
+                numbers.add(b);
+                numbers.add(c);
 
-        System.out.println(numbers.reversed());
+                System.out.println(numbers.reversed());
+                validInput = true;
+            }catch (InputMismatchException e){
+                System.out.println("Please type a valid number.\n");
+                sc.nextLine();
+            }
+        }
         sc.close();
     }
 }
