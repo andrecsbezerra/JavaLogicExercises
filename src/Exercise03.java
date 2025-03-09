e04e1d81faf52345dfca2bd0952358c83e907e3c
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-09 13:46:21 -0300

    feat: add exercise 03

---
+++ src/Exercise03.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        boolean validInput = false;
+
+        while (!validInput){
+            try {
+                System.out.println("Type the first number: ");
+                a = sc.nextInt();
+
+                System.out.println("Type the second number: ");
+                b = sc.nextInt();
+
+                if (a == b){
+                    c = a + b;
+                    System.out.printf("The sum of (%d + %d) is: %d.", a,b,c);
+                }else {
+                    c = a * b;
+                    System.out.printf("The multiplication of (%d * %d) is %d.", a,b,c);
+                }
+
+                validInput = true;
+            }catch (InputMismatchException e){
+                System.out.println("Please type a valid number.");
+                System.out.println();
+                sc.nextLine();
+            }catch(Exception e){
+                System.out.println("Something went wrong");
+                sc.nextLine();
+            }
+        }
+        sc.close();
+    }
+}
