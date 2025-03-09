b2e3e64a28331e85f51edfa6d1b8cdf704153a5e
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-09 18:41:55 -0300

    refactor: add double Type to valuePlusFivePercent

--- src/Exercise06.java
+++ src/Exercise06.java
@@ -5,14 +5,13 @@ public class Exercise06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double value;
-        double valuePlusFivePercent = 0;
         boolean validInput = false;
         while (!validInput){
             try {
                 System.out.println("Type a number: ");
                 value = sc.nextDouble();
 
-                valuePlusFivePercent = value * 1.05;
+                double valuePlusFivePercent = value * 1.05;
                 System.out.printf("Your number +5%% is:  %.2f ", valuePlusFivePercent);
 
                 validInput = true;
