c68c2dd923af3bf486ad1d7de0ce22ff82c766c0
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-09 13:18:51 -0300

    feat: add exercise 02

---
+++ src/Exercise02.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        boolean validInput = false;
+
+        while (!validInput){
+            try {
+                System.out.println("Type a number: ");
+                num = sc.nextInt();
+
+                if (num > 0){
+                    System.out.println("The number is Positive");
+                }else {
+                    System.out.println("The number is Negative");
+                }
+
+                if (num % 2 == 0){
+                    System.out.println("The number is Even");
+                }else{
+                    System.out.println("The number is Odd");
+                }
+                validInput = true;
+            }catch (InputMismatchException e){
+                System.out.println("Please type a valid number");
+                sc.nextLine();
+            }catch(Exception e){
+                System.out.println("Something went wrong");
+                sc.nextLine();
+            }
+        }
+        sc.close();
+    }
+}
