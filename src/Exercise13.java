f51c7ec09cf670badc1392ddee27f95aac7817dd
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-09 21:00:00 -0300

    feat: add exercise 13

---
+++ src/Exercise13.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean validInput = false;
+        while (!validInput){
+            try{// Ask for the user's name
+                System.out.print("Please enter your name: ");
+                String name = sc.nextLine();
+
+                // Ask for the user's age
+                System.out.print("Please enter your age: ");
+                int age = sc.nextInt();
+
+                // Determine if the user is above or underage
+                if (age >= 18) {
+                    System.out.printf("%s, you are adult.\n", name);
+                } else {
+                    System.out.printf("%s, you are underage.\n", name);
+                }
+                validInput = true;
+            }catch (InputMismatchException e){
+                System.out.println("Check The Fields Again.\n");
+                sc.nextLine();
+            }
+        }
+        sc.close();
+    }
+}
