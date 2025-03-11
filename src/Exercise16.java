d7dd4a31ab9051e5a26896fe71279d07745ccaae
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-10 23:59:21 -0300

    feat: add exercise 16

---
+++ src/Exercise16.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Type the first value: ");
+            int n1 = sc.nextInt();
+            System.out.println("Type the first value: ");
+            int n2 = sc.nextInt();
+            System.out.println("Type the first value: ");
+            int n3 = sc.nextInt();
+            if(n1 + n2 + n3 != 180){
+                System.out.println("Enter a valid value for a triangle(180°)");
+            }else if (n1 == n2 && n2 == n3){
+                System.out.println("Equilateral Triangle");
+            }else if(n1 == n2 || n2 == n3 || n1 == n3){
+                System.out.println("Isosceles Triangle");
+            } else if (n1 != n2 && n2 != n3 && n1 != n3 ) {
+                System.out.println("Scalene Triangle");
+            }
+        }catch (InputMismatchException e ){
+            System.out.println("Type a valid value");
+        }finally {
+            sc.close();
+        }
+    }
+}
