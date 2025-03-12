6ade7e46362839dc5fc9df4b9d1fd74cb40c86bd
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-12 17:59:12 -0300

    refactor: remove salary redundant variable

--- src/Exercise23.java
+++ src/Exercise23.java
@@ -1,11 +1,10 @@
 public class Exercise23 {
     public static void main(String[] args) {
-        double salary = 0;
         double hourlyRate = 40;
         int classesTaughtInTheMonth = 230;
         double inssDiscountPercentage = 0.11;
 
-        salary = (hourlyRate * classesTaughtInTheMonth) - inssDiscountPercentage;
+        double salary = (hourlyRate * classesTaughtInTheMonth) - inssDiscountPercentage;
         System.out.println("The professor liquid salary is: $" + salary);
     }
 }
