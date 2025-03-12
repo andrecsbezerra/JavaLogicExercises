6c40e9959f51ddf2d45ffab2c6d7dccc1e2e7760
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-12 17:25:00 -0300

    feat: add exercise 20

---
+++ src/Exercise20.java
@@ -0,0 +1,8 @@
+public class Exercise20 {
+    public static void main(String[] args) {
+        int v = 5;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(v + " x " + i + " = " + v * i);
+        }
+    }
+}
