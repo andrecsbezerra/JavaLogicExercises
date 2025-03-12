5cb3cadebd4926380449acda480800d686bc7d9b
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-12 18:08:19 -0300

    refactor: add brute and liquid salary

--- src/Exercise23.java
+++ src/Exercise23.java
@@ -2,9 +2,11 @@ public class Exercise23 {
     public static void main(String[] args) {
         double hourlyRate = 40;
         int classesTaughtInTheMonth = 230;
-        double inssDiscountPercentage = 0.11;
+        double inssDiscountPercentage = 0.89;
 
-        double salary = (hourlyRate * classesTaughtInTheMonth) - inssDiscountPercentage;
-        System.out.println("The professor liquid salary is: $" + salary);
+        double bruteSalary = hourlyRate * classesTaughtInTheMonth;
+        double liquidSalary = (hourlyRate * classesTaughtInTheMonth) * inssDiscountPercentage;
+        System.out.println("The professor brute salary is: $" + bruteSalary);
+        System.out.println("The professor liquid salary is: $" + liquidSalary);
     }
 }
