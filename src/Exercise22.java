7748eca508b8031a8e441adf79f0bb77845f9686
André Bezerra <andrebezerracomercial@gmail.com>
2025-03-12 17:32:02 -0300

    feat: add exercise 22

---
+++ src/Exercise22.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Type the first value: ");
+            int n1 = sc.nextInt();
+            System.out.println("Type the second value value: ");
+            int n2 = sc.nextInt();
+
+            int division = n1 / n2;
+            int remainder = n1 % n2;
+
+            System.out.println("Quocient: " + division);
+            System.out.println("Remainder: " + remainder);
+        }catch (InputMismatchException e ){
+            System.out.println("Type a valid value");
+        }finally {
+            sc.close();
+        }
+    }
+}
